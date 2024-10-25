77f1a6a69434d409b60cc4817f7ab8c196020eff
Laura Blanco <laurablancoc27@gmail.com>
2024-10-25 16:34:55 -0500

    pokemon-UI

--- src/ListPokemon.js
+++ src/ListPokemon.js
@@ -1,36 +1,30 @@
 import { LitElement, html } from 'lit-element';
 import { getComponentSharedStyles } from '@bbva-web-components/bbva-core-lit-helpers';
 import styles from './list-pokemon.css.js';
-import "@bbva-experience-components/bbva-button-default/bbva-button-default.js"
+import "@bbva-experience-components/bbva-button-default/bbva-button-default.js";
+import "@bbva-web-components/bbva-foundations-spinner/bbva-foundations-spinner.js";
+import "@bbva-experience-components/bbva-type-text/bbva-type-text.js"
+import { BbvaCoreIntlMixin } from '@bbva-web-components/bbva-core-intl-mixin';
+import "@bbva-web-components/bbva-web-card-product/bbva-web-card-product.js"
 
-/**
- * ![LitElement component](https://img.shields.io/badge/litElement-component-blue.svg)
- *
- * This component ...
- *
- * Example:
- *
- * ```html
- *   <list-pokemon></list-pokemon>
- * ```
- */
-export class ListPokemon extends LitElement {
+export class ListPokemon extends BbvaCoreIntlMixin(LitElement) {
+  
+
   static get properties() {
     return {
-      items: { type: Array }, // Propiedad items
-      arrayPokemon: { type: Array }, // Propiedad a
-      name: {
-        type: String,
-      },
+      items: { type: Array }, 
+      arrayPokemon: { type: Array }, 
+      name: { type: String },
+      loading: { type: Boolean }, 
     };
   }
 
   constructor() {
     super();
     this.items = [];
-    this.arrayPokemon = []; // Inicializamos arrayPokemon
-    
-    this.fetchPokemon(); // Llamamos al método para obtener los Pokémon al cargar la página
+    this.arrayPokemon = [];
+    this.loading = true;
+    this.fetchPokemon(); 
   }
 
   static get styles() {
@@ -40,20 +34,14 @@ export class ListPokemon extends LitElement {
     ];
   }
 
-  
-
-  _updateCustomerData(customerList) {
-    this.items = customerList;
-  }
-
-  async fetchPokemon() {
+  async fetchPokemon(limit = 100, offset = 0) {
     try {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
-      const data = await response.json(); // Convertimos la respuesta a JSON
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+      const data = await response.json(); 
       const pokemonList = data.results; // Accedemos a la lista de Pokémon
       this.arrayPokemon = await Promise.all(
-        pokemonList.map(async(pokemon) => {
-          const res = await fetch(pokemon.url); // aqui se obtienen más detalles de cada Pokémon
+        pokemonList.map(async (pokemon) => {
+          const res = await fetch(pokemon.url); // obtener más detalles de cada Pokémon
           const pokemonDetails = await res.json();
           return {
             name: pokemonDetails.name,
@@ -62,28 +50,43 @@ export class ListPokemon extends LitElement {
           };
         })
       );
+      this.loading = false;
     } catch (error) {
       console.error('Error al obtener los Pokémon:', error);
+      this.loading = false; 
     }
   }
 
   render() {
     return html`
-      <div role="list" class="card-container"> 
-        ${this.arrayPokemon.length > 0
-    ? this.arrayPokemon.map(pokemon => html`
-              <div class="card"> <!-- Clase card para dar estilo -->
-                <h2>Pokémon: ${pokemon.name}</h2>
-                <div @click="${() => this.navigateToDetails(pokemon.name)}"
-                        @keydown="${(e) => this._handleKeyDown(e, pokemon.name)}"
-                        class="pokemon-image-button">
-                  <img src="${pokemon.image}" alt="${pokemon.name}" class="pokemon-image">
+    <div class=container>
+      ${this.loading 
+        ? html`<bbva-foundations-spinner></bbva-foundations-spinner>` 
+        : html`
+          <div>
+            <bbva-type-text text="${this.t('pokemon-title')}" size="3XL"></bbva-type-text>
+          </div>
+          <div role="list" class="card-container"> 
+            ${this.arrayPokemon.length > 0
+              ? this.arrayPokemon.map(pokemon => html`
+                <div class="card">
+                  <bbva-type-text text="${pokemon.name}"></bbva-type-text>
+                  <div @click="${() => this.navigateToDetails(pokemon.name)}"
+                       @keydown="${(e) => this._handleKeyDown(e, pokemon.name)}"
+                       class="pokemon-image-button">
+                    <img src="${pokemon.image}" alt="${pokemon.name}" slot="media" class="pokemon-image">
+                  </div>
+                  <div>
+                    ${pokemon.types.split(', ').map(type => html`
+                      <bbva-type-text class="pokemon-type ${type.toLowerCase()}" text="${type}"></bbva-type-text>
+                    `)}
+                  </div>
+                  <bbva-button-default variant="secondary" text="${this.t('pokemon-button')}" ></bbva-button-default>
                 </div>
-                <p>Tipo: ${pokemon.types}</p>
-                <bbva-button-default text="Evolucion"></bbva-button-default>
-              </div>
-            `)
-    : html`<p>Cargando datos de Pokémon...</p>`}
+              `)
+              : html`<p>No se encontraron Pokémon.</p>`}
+          </div>
+        `}
       </div>
     `;
   }
--- test/list-pokemon.test.js
+++ test/list-pokemon.test.js
@@ -1,26 +1,54 @@
-import {
-  html,
-  fixture,
-  assert,
-  fixtureCleanup
-} from '@open-wc/testing';
-import '../list-pokemon.js';
+import { html, fixture, assert, fixtureCleanup } from '@open-wc/testing';
+import '../list-pokemon.js'; // Asegúrate de que la ruta sea correcta
 
 suite('ListPokemon', () => {
   let el;
 
   teardown(() => fixtureCleanup());
 
-  suite('default', () => {
-    setup(async () => {
-      el = await fixture(html`
-        <list-pokemon></list-pokemon>
-      `);
-      await el.updateComplete;
+  setup(async () => {
+    el = await fixture(html`<list-pokemon></list-pokemon>`);
+    await el.updateComplete;
+  });
+
+  test('You should set the initial property values.', () => {
+    assert.deepEqual(el.items, []);
+    assert.deepEqual(el.arrayPokemon, []);
+    assert.equal(el.name, undefined); // Inicialmente no hay un valor asignado
+  });
+
+  test('Should fetch Pokémon data successfully.', async () => {
+    // Simulamos que fetch se resuelve correctamente
+    const fetchStub = sinon.stub(window, 'fetch').resolves({
+      json: () => Promise.resolve({ results: [{ name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' }] }),
     });
 
-    test('a11y', async () => {
-      await assert.isAccessible(el);
-    });
+    await el.fetchPokemon();
+    await el.updateComplete;
+
+    assert.isAbove(el.arrayPokemon.length, 0);
+    assert.equal(el.arrayPokemon[0].name, 'bulbasaur');
+
+    fetchStub.restore();
+  });
+
+  test('Render method', () => {
+    el.arrayPokemon = [
+      { name: 'bulbasaur', image: 'https://img.pokemondb.net/sprites/home/normal/bulbasaur.png', types: 'grass, poison' },
+      { name: 'ivysaur', image: 'https://img.pokemondb.net/sprites/home/normal/ivysaur.png', types: 'grass, poison' },
+    ];
+    el.requestUpdate(); // Notificamos que la propiedad ha cambiado
+    assert.equal(el.shadowRoot.querySelectorAll('.card').length, 2);
+  });
+
+  test('Should handle error when fetching Pokémon data.', async () => {
+    // Simulamos que fetch lanza un error
+    const fetchStub = sinon.stub(window, 'fetch').rejects(new Error('Fetch error'));
+
+    await el.fetchPokemon();
+    await el.updateComplete;
+
+    assert.deepEqual(el.arrayPokemon, []); // Verificamos que no haya datos
+    fetchStub.restore();
   });
 });
